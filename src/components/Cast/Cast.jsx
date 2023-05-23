42334be49db67c1c1d891204fd6a26d021fbc29b
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-05-23 23:53:53 +0300

    removed unused imports and add axios

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Cast = () => {
   return <div>Cast</div>;
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Reviews = () => {
   return <div>Reviews</div>;
 };
