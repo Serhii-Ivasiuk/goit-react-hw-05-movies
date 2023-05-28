1137aae1ef34b00c4acf7d5aabbbf54e4fe568f6
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-05-28 21:12:37 +0300

    add loader wrapper

---
+++ src/components/Loader/Lader.styled.jsx
@@ -0,0 +1,16 @@
+// Libs
+import styled from '@emotion/styled';
+
+export const LoaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 80px;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 1000;
+`;
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,14 @@
 // Libs
 import { BeatLoader } from 'react-spinners';
+// Styled components
+import { LoaderWrapper } from './Lader.styled';
 
 const Loader = () => {
-  return <BeatLoader color="#36d7b7" />;
+  return (
+    <LoaderWrapper>
+      <BeatLoader color="#36d7b7" />
+    </LoaderWrapper>
+  );
 };
 
 export default Loader;
